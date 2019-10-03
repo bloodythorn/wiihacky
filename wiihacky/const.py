604b68e05577cf67827de16083412020da4905ee
bloodythorn <bloodythorn@protonmail.com>
2019-10-03 17:40:53 -0600

    added constants

--- wiihacky/const.py
+++ wiihacky/const.py
@@ -1,3 +1,20 @@
 """Constants for WiiHacky."""
 
 __version__ = "v0.0.2"
+
+# Logger Related
+LOG_FORMAT_STRING = '%(asctime)s:%(name)s:%(levelname)s:%(message)s'
+
+# Output Related
+TXT_INIT_LOGGED_IN = 'logged in as %s'
+TXT_INIT_LOGGER_SUCC = 'successfully initialized logger.'
+TXT_INIT_REDDIT_SUCC = 'successfully initialized reddit instance.'
+TXT_SCRP_USER = 'scraping user...'
+TXT_SCRP_INBOX = 'scraping inbox...'
+TXT_SCRP_COMPLETE = 'scraping complete.'
+TXT_SCRP_NOT_SUPPORTED = 'general scraping does not support type %s'
+TXT_RUN_INTERUPT = 'ctrl-c interupt detected.'
+TXT_RUN_START_BOT = 'starting bot. press ctrl-c to interupt.'
+
+# File Names
+FILE_DEFAULT_CONFIG = 'config.yml'
