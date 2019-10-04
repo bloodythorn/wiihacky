0d3e7249ef899cc29209b7446658a1005c8febf2
bloodythorn <bloodythorn@protonmail.com>
2019-10-03 19:20:29 -0600

    uniformed const text naming convention

--- wiihacky/const.py
+++ wiihacky/const.py
@@ -5,16 +5,22 @@ __version__ = "v0.0.2"
 # Logger Related
 LOG_FORMAT_STRING = '%(asctime)s:%(name)s:%(levelname)s:%(message)s'
 
-# Output Related
-TXT_INIT_LOGGED_IN = 'logged in as %s'
-TXT_INIT_LOGGER_SUCC = 'successfully initialized logger.'
-TXT_INIT_REDDIT_SUCC = 'successfully initialized reddit instance.'
-TXT_SCRP_USER = 'scraping user...'
-TXT_SCRP_INBOX = 'scraping inbox...'
-TXT_SCRP_COMPLETE = 'scraping complete.'
-TXT_SCRP_NOT_SUPPORTED = 'general scraping does not support type %s'
-TXT_RUN_INTERUPT = 'ctrl-c interupt detected.'
-TXT_RUN_START_BOT = 'starting bot. press ctrl-c to interupt.'
-
 # File Names
 FILE_DEFAULT_CONFIG = 'config.yml'
+
+# WiiHacky module txt constants
+WH_INIT_LOGGED_IN = 'logged in as %s.'
+WH_INIT_LOGGER_SUCC = 'successfully initialized logger.'
+WH_INIT_REDDIT_SUCC = 'successfully initialized reddit instance.'
+WH_INIT_SCRAPER = 'successfully initialized scraper.'
+WH_RUN_INTERUPT = 'ctrl-c interupt detected.'
+WH_RUN_START_BOT = 'starting bot. press ctrl-c to interupt.'
+
+# Scraper module txt constants
+SCRAPE_UTC_STAMP = 'utc_timestamp'
+SCRAPE_USER = 'scraping user...'
+SCRAPE_INBOX = 'scraping inbox...'
+SCRAPE_COMPLETE = 'scraping complete.'
+SCRAPE_TYPE_UNSUPPORTED = 'general scraping does not support type %s.'
+SCRAPE_TYPE = 'type'
+SCRAPE_ID = 'id'
