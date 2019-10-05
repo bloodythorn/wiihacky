f01baa797dabf2213575f11c35dd25d648640bb8
bloodythorn <bloodythorn@protonmail.com>
2019-10-05 16:52:39 -0600

    finished multireddit scraper cleaned up other scrapers #11 done

--- wiihacky/scraper.py
+++ wiihacky/scraper.py
@@ -1,15 +1,8 @@
 """Scrapper Module."""
 
-import logging as lg
 import time as tm
-import praw as pr
-
 
 import wiihacky.const as const
-
-lg.basicConfig(
-    level=lg.INFO,
-    format=const.LOG_FORMAT_STRING)
 
 
 class Scraper:
@@ -17,90 +10,96 @@ class Scraper:
 
     def __init__(self):
         """Initialize the scraper."""
-        # Logger
-        self.log = lg.getLogger(self.__class__.__name__)
 
-    def scrape(self, target):
-        """Scrape object.
-
-        A general scrape function. It can be given a single or list or tuple
-        of praw objects and it will return either a single dict, or a list
-        of dicts if more than one object was given.
-        """
-        if isinstance(target, (list, tuple)):
-            lin = [self.scrape(item) for item in target]
-            lout = []
-            while len(lin) > 0:
-                item = lin.pop()
-                if isinstance(item, list):
-                    lin = lin + item
-                else:
-                    lout.append(item)
-            return lout
-
-        if isinstance(target, pr.models.user.User):
-            return self.scrape_user(target)
-
-        if isinstance(target, pr.models.inbox.Inbox):
-            return self.scrape_inbox(target)
-
-        self.log.error(const.SCRAPE_TYPE_UNSUPPORTED, type(target))
-        return {}
+    @staticmethod
+    def get_timestamp():
+        """Obtain a timestamp in utc unix."""
+        return (const.SCRAPE_UTC_STAMP, int(tm.time()))
 
     def scrape_inbox(self, inbox):
         """Scrape inbox.
 
-        This function will scrape the inbox and make sure it is up to date.
+        This function will scrape the inbox return a data structure reflecting
+        its state.
+        Anything it had to evaluate (make a reddit request for) is returned
+        also.
 
         Return
         ------
-        This will return a dictionary with all inbox data.
+        This will return a tuple containing a dictionary with the scraped
+        information, as well as a list of all praw objects that had to be
+        evaluated to complete the task.
 
         """
-        # Log
-        info = self.log.info
-        info(const.SCRAPE_INBOX)
-
         recv = list(inbox.all())
         sent = list(inbox.sent())
+        st_name, st_time = self.get_timestamp()
 
         output = {
             const.SCRAPE_TYPE: inbox.__class__.__name__,
-            const.SCRAPE_UTC_STAMP: int(tm.time()),
+            st_name: st_time,
             inbox.all.__name__: [a.id for a in recv],
             inbox.sent.__name__: [a.id for a in sent]}
 
-        xtras = self.scrape([recv, sent])
-        info(const.SCRAPE_COMPLETE)
-        return xtras + [output] if isinstance(xtras, list) else [xtras, output]
+        xtras = recv + sent
+        return (output, xtras)
+
+    def scrape_multireddit(self, multi):
+        """Scrape a multi reddit.
+
+        This function will scrape the multiredit return a data structure
+        reflecting its state.
+        Anything it had to evaluate (make a reddit request for) is returned
+        also.
+
+        Return
+        ------
+        This will return a tuple containing a dictionary with the scraped
+        information, as well as a list of all praw objects that had to be
+        evaluated to complete the task.
+
+        """
+        output = dict(vars(multi))
+        del output['_reddit']
+        subs = output['subreddits']
+        del output['subreddits']
+        auth = output['_author']
+        del output['_author']
+        st_name, st_time = self.get_timestamp()
+        output[st_name] = st_time
+
+        xtras = subs + [auth]
+        return (output, xtras)
 
     def scrape_user(self, user):
         """Scrape user.
 
-        This function will scrape the user and update the user record.
+        This function will scrape the user return a data structure reflecting
+        its state.
+        Anything it had to evaluate (make a reddit request for) is returned
+        also.
 
         Return
         ------
-        This will return a dictionary with all user data.
+        This will return a tuple containing a dictionary with the scraped
+        information, as well as a list of all praw objects that had to be
+        evaluated to complete the task.
 
         """
-        # Log
-        info = self.log.info
-
-        info(const.SCRAPE_USER)
-
         # Pull all info
+        me = user._me
         blck = list(user.blocked())
         frnd = list(user.friends())
         mods = list(user.moderator_subreddits())
         mult = list(user.multireddits())
         subs = list(user.subreddits())
+        st_name, st_time = self.get_timestamp()
 
         # Put it in data
         output = {
-            const.SCRAPE_UTC_STAMP: int(tm.time()),
+            st_name: st_time,
             const.SCRAPE_TYPE: user.__class__.__name__,
-            const.SCRAPE_ID: user.me().id,
+            const.SCRAPE_ID: me.id,
             user.blocked.__name__: [a.id for a in blck],
             user.friends.__name__: [a.id for a in frnd],
             user.karma.__name__: user.karma(),
@@ -109,6 +108,5 @@ class Scraper:
             user.preferences.__class__.__name__: dict(user.preferences()),
             user.subreddits.__name__: [a.id for a in subs]}
 
-        xtras = self.scrape([blck, frnd, mods, mult, subs])
-        info(const.SCRAPE_COMPLETE)
-        return xtras + [output] if isinstance(xtras, list) else [xtras, output]
+        xtras = blck + frnd + mods + mult + subs + [me]
+        return (output, xtras)
