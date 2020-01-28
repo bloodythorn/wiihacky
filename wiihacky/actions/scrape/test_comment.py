f50728b7961bdad5c1806797e0c9e87fb488cd2a
bloodythorn <bloodythorn@protonmail.com>
2020-01-27 18:31:02 -0700

    added loop to do 10 comments

--- wiihacky/actions/scrape/test_comment.py
+++ wiihacky/actions/scrape/test_comment.py
@@ -1,12 +1,13 @@
 def test_comment(wh):
-   wh.log.info('-- Comment Test -')
-   try:
-      from wiihacky.actions.scrape import ScrapeComment
-      from random import choice
-      comment = choice(list(wh.reddit.random_subreddit().comments()))
-      wh.log.info('-- Target: {} -'.format(comment.id))
-      ac = ScrapeComment(wh.log, comment)
-      ac.execute()
-   except Exception as e:
-      wh.log.error('TEST**** ScrapeComment failed!', e)
-      assert False
+    wh.log.info('-- Comment Test -')
+    try:
+        from wiihacky.actions.scrape import ScrapeComment
+        from random import choice
+        for a in range(10):
+            comment = choice(list(wh.reddit.random_subreddit().comments()))
+            wh.log.info('-- Target: {} -'.format(comment.id))
+            ac = ScrapeComment(wh.log, comment)
+            ac.execute()
+    except Exception as e:
+        wh.log.error('TEST**** ScrapeComment failed!', e)
+        assert False
