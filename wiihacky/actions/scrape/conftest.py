43364d23861a1857e833962bbd2c5ba340bd47a7
bloodythorn <bloodythorn@protonmail.com>
2020-02-10 15:20:03 -0700

    cleaned up conftest

--- wiihacky/actions/scrape/conftest.py
+++ wiihacky/actions/scrape/conftest.py
@@ -1,12 +1,22 @@
-# If the configuration is not set to run from the project run directory,
-# this test will not work.
+"""
+This is the test for actions.scrape.comment.
+
+It will scrape ten comments. Fail condition is if it gives an exception.
+
+All tests that use it need to be set to run from the project's 'run' directory
+or the instance will not be able to find its configuration.
+"""
 
 import pytest
 
 
 @pytest.fixture
 def wh():
-    """ This function returns an instance of the bot. """
+    """
+    This function returns an instance of the bot.
+
+    :return: WiiHacky() instance.
+    """
     from wiihacky import WiiHacky
     wh = WiiHacky()
     return wh
