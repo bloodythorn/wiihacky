ab6a3a88117a5816b2d22e3d36f64455f4c367b4
bloodythorn <bloodythorn@protonmail.com>
2020-02-10 23:14:42 -0700

    finalized scrape subreddit

--- wiihacky/actions/scrape/subreddit.py
+++ wiihacky/actions/scrape/subreddit.py
@@ -1,42 +1,41 @@
 import logging as lg
 
 from praw.models import Subreddit
-from praw import Reddit
 
 import wiihacky.actions.scrape.constants as const
-import actions
+import wiihacky
 
 
-class ScrapeSubreddit(actions.Action):
+class ScrapeSubreddit(wiihacky.actions.Action):
     """This action when given a subreddit will scrape and save the data."""
-
-    TXT_AC = const.TXT_START + ' ' + const.TXT_SUBREDDIT
 
     def __init__(self, log: lg.Logger, subreddit_name: str):
         """Initialize the action."""
-        actions.Action.__init__(self, log)
+        super().__init__(log)
+        self.action_text = const.TXT_START + ' ' + const.TXT_SUBREDDIT
         self.subreddit_name = subreddit_name
         self.data = {}
 
-    def execute(self, reddit: Reddit):
+    def execute(self, wh: wiihacky.WiiHacky):
         """Execute Action."""
+        reddit = wh.reddit
         try:
             subreddit = reddit.subreddit(self.subreddit_name)
 
             # Scrape
             try:
-                self.log.info(self.TXT_AC + '.')
+                self.log.info(self.action_text + '.')
                 self.data = self.scrape(subreddit)
             except Exception as e:
                 self.log.error(
-                    const.TXT_ERR_EXCEPT.format(self.TXT_AC + ':', e))
+                    const.TXT_ERR_EXCEPT.format(self.action_text + ':', e))
                 raise e
 
             # Save
             try:
                 self.log.info(
                     const.TXT_SAVING.format(const.TXT_SUBREDDIT.capitalize()))
-                actions.scrape.save_data(self.data)
+                wiihacky.actions.scrape.save_data(self.data)
                 self.executed = True
             except Exception as e:
                 self.log.error(
@@ -49,24 +48,22 @@ class ScrapeSubreddit(actions.Action):
             raise e
 
         # End of Action
-        actions.action_concluded(self.log, self.TXT_AC, self.executed)
+        self.action_concluded()
 
     @staticmethod
     def scrape(subr: Subreddit):
-        """Scrape a subreddit.
+        """
+        Scrape a subreddit.
 
         This function will scrape a subreddit and return a data structure
         reflecting its state.
 
-        Return
-        ------
-        a dict with scraped data.
-
+        :param subr: Subreddit data type from praw
+        :return: Dict with scraped data.
         """
-
-        actions.scrape.fetch(subr)
+        wiihacky.actions.scrape.fetch(subr)
         output = dict(vars(subr))
-        actions.scrape.prep_dict(output, const.TXT_SUBREDDIT)
+        wiihacky.actions.scrape.prep_dict(output, const.TXT_SUBREDDIT)
         output[const.TXT_PATH] = output['_' + const.TXT_PATH]
         output.update([
             (const.TXT_COMMENTS, [a.id for a in subr.comments()]),
@@ -77,4 +74,4 @@ class ScrapeSubreddit(actions.Action):
             (subr.rising.__name__, [a.id for a in subr.rising()]),
             (subr.top.__name__, [a.id for a in subr.top()]),
         ])
-        return actions.scrape.strip_all(output)
+        return wiihacky.actions.scrape.strip_all(output)
--- wiihacky/actions/scrape/test_subreddit.py
+++ wiihacky/actions/scrape/test_subreddit.py
@@ -1,5 +1,9 @@
 def test_subreddit(wh):
-    # Subreddit
+    """
+    Scrape 10 random subreddits. Failure on exception.
+    :param wh:
+    :return:
+    """
     wh.log.info('-- Subreddit Test -')
     try:
         from wiihacky.actions.scrape import ScrapeSubreddit
@@ -7,7 +11,7 @@ def test_subreddit(wh):
             subreddit = wh.reddit.random_subreddit()
             wh.log.info('-- Target: {} -'.format(subreddit.display_name))
             ac = ScrapeSubreddit(wh.log, subreddit.display_name)
-            ac.execute(wh.reddit)
+            ac.execute(wh)
     except Exception as e:
         wh.log.error('TEST**** ScrapeSubreddit failed!', e)
         assert False
